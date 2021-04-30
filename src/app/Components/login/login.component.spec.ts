3361fe99104be93ba96255a05eb2b8a834f3e847
Maximiliano Brun <brunmaxi6@gmail.com>
2021-04-30 00:40:01 -0300

    router testing

--- src/app/Components/login/login.component.spec.ts
+++ src/app/Components/login/login.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import {} from '@angular/common/http/testing'
+import { HttpClient } from '@angular/common/http';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -11,6 +13,7 @@ describe('LoginComponent', () => {
       declarations: [ LoginComponent ]
     })
     .compileComponents();
+    TestBed.inject(HttpClient);
   });
 
   beforeEach(() => {
--- src/app/app-routing.component.spec.ts
+++ src/app/app-routing.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { TestBed, fakeAsync, tick, async, ComponentFixture } from '@angular/core/testing';
 import { Location } from '@angular/common'
 import { Router } from '@angular/router'
 import { RouterTestingModule } from '@angular/router/testing'
@@ -8,36 +8,52 @@ import { AppComponent } from './app.component';
 
 import {routingComponents, routes} from './app-routing.module'
 import { AppModule } from './app.module';
-
-
-    let location: Location;
-    let router: Router;
-    let fixture;
+import { DebugElement } from '@angular/core';
 
     describe('Routing Component', () => {
 
-    beforeAll(() => {
-        router = TestBed.get(Router);
-        location = TestBed.get(Location);
-        fixture = TestBed.createComponent(AppComponent);
-        router.initialNavigation();
-        localStorage.removeItem('userId');
-    });
-    
-    beforeEach(async () => {
-        await TestBed.configureTestingModule({
-            imports: [RouterTestingModule.withRoutes(routes), AppModule],
-            declarations: [
-                routingComponents
-            ],
-            providers: [Router],
-        }).compileComponents();
-    });
+        let location: Location;
+        let router: Router;
+        let component: AppComponent;
+        let fixture: ComponentFixture<AppComponent>;
+        //let debug: DebugElement
+        
+        beforeEach(async () => {
+            await TestBed.configureTestingModule({
+                imports: [RouterTestingModule.withRoutes(routes)],
+                declarations: [
+                    routingComponents
+                ],
+                providers: [],
+            }).compileComponents();
+        });
 
-    it('navigate to "" redirects you to "login"', fakeAsync(() => {
-      router.navigate(['']);
-      tick();
-      expect(location.path()).toBe('/login');  
-    }))
+        beforeEach( () => {
+            router = TestBed.inject(Router);
+            location = TestBed.inject(Location);
+            fixture = TestBed.createComponent(AppComponent);
+            router.initialNavigation();
+            localStorage.removeItem('userId');
+        })
+
+        it('navigate to "" should redirects you to "login"', fakeAsync(() => {
+        router.navigate(['']);
+        tick();
+        expect(location.path()).toBe('/login');  
+        }))
+
+        it('navigate to "/pokemons" should redirects you to "login" if you dont have userId', fakeAsync(() => {
+            router.navigate(['pokemons']);
+            tick();
+            expect(location.path()).toBe('/pokemons');  
+        }))
+
+        it('navigate to "/pokemons" should stay in "/pokemons" if you have userId', fakeAsync(() => {
+            localStorage.setItem('userId', '1');
+            router.navigate(['pokemons']);
+            tick();
+            localStorage.removeItem('userId');
+            expect(location.path()).toBe('/pokemons');
+        }))
   
 });
